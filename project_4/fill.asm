fc8fffbfc745683147e95e2e63c44b64e5094dc2
Mirunaelenav26 <miruna.vladoiu05@gmail.com>
2025-05-27 13:10:21 +0300

    Add files via upload

---
+++ project_4/fill.asm
@@ -0,0 +1,45 @@
+
+
+  @place 
+  M=0
+  (LOOP)
+  @KBD 
+  D=M
+  @WHITE
+  D;JEQ 
+  @BLACK
+  0;JMP 
+  (WHITE)
+  @place
+  D=M 
+  @LOOP
+  D;JLT 
+  @place
+  D=M
+  @SCREEN
+  A=D+A
+  M=0
+  @place
+  M=M-1
+  @LOOP
+  0;JMP
+  (BLACK)
+  @place
+  D=M
+  @8192 
+  D=D-A
+  @LOOP
+  D;JGE
+  @place
+  D=M
+  @SCREEN
+  A=D+A 
+  M=-1 
+  @place
+  M=M+1 
+  @LOOP
+  0;JMP 
+
+(END)
+  @END
+  0;JMP 
